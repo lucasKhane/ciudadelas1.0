6b3317592e51583e7b73449d58bc76e9e494727a
lucasKhane <dani.kn17.rjch@gmail.com>
2016-04-12 13:16:56 +0200

    Funcion render realizada con handlers y deferred

--- game.js
+++ game.js
@@ -1,22 +1,52 @@
 //Inicializamos todos los elementos del juego (por ejemplo el canvas)
 //Objetos del juego
 
-var ctx;
+var ctx; //No me acaba de gustar tener esta variable global
 
 function render(source,positionX,positionY){
-  var ready = false;
-  var image = new Image();
-  image.onload = function(){
-    ready = true;
-    console.log(ready);
-  };
+  $.loadImage = function(source) {
+    // Define a "worker" function that should eventually resolve or reject the deferred object.
+    var loadImage = function(deferred) {
+      var image = new Image();
+      // Set up event handlers to know when the image has loaded
+      // or fails to load due to an error or abort.
+      image.onload = loaded;
+      image.onerror = errored; // URL returns 404, etc
+      image.onabort = errored; // IE may call this if user clicks "Stop"
+      // Setting the src property begins loading the image.
+      image.src = source;
 
-  image.src = source;
-  console.log(ready);
-  if (ready){
+      function loaded() {
+        unbindEvents();
+        // Calling resolve means the image loaded sucessfully and is ready to use.
+        console.log("Imagen cargada: "+source);
+        //Curioso: si pones un comentario tras el resolve, se ejecuta primero el .done
+        //antes que el comentario, lo que quiere decir que done se ejecuta inmediatamente despues!!
+        //Incluso antes que un insignificante comentario
+        deferred.resolve(image);
+      }
+      function errored() {
+        unbindEvents();
+        // Calling reject means we failed to load the image (e.g. 404, server offline, etc).
+        deferred.reject(image);
+        console.log("Error al cargar la imagen: "+source);
+      }
+      function unbindEvents() {
+        // Ensures the event callbacks only get called once.
+        image.onload = null;
+        image.onerror = null;
+        image.onabort = null;
+      }
+    };
+    // Create the deferred object that will contain the loaded image.
+    // We don't want callers to have access to the resolve() and reject() methods,
+    // so convert to "read-only" by calling `promise()`.
+    return $.Deferred(loadImage).promise();
+  };
+  $.loadImage(source).done(function(image){
     ctx.drawImage(image,positionX, positionY);
-  }
-  console.log(ready);
+    console.log("Imagen dibujada: "+positionX+","+positionY);
+  });
 }
 
 function cartaMazo(nombre, coste, color, descripcion){
@@ -194,7 +224,7 @@ function initializeGame() {
 
   //initCartasMazo();
   //shuffle(cartasMazo);
-  console.log("Fin ejecucion. No puede haber nada despues de esto");
+  console.log("Fin ejecucion initializeGame()");
 }
 
 /**
