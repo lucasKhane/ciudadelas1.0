acf787c09e06ec9dd8d30e310d2ed1c44c057eca
lucasKhane <dani.kn17.rjch@gmail.com>
2016-04-13 03:57:12 +0200

    Cambios y añadida lista de tareas que faltan en la guia. Aunque alguna cosa se me pasara...

--- inteligencia artificial.js
+++ inteligencia artificial.js
@@ -6,19 +6,19 @@
 $(document).ready(function(){
   $("canvas").click(function (e) {
     //e es el evento click y el this es el objeto clickeado
-    //Mazo
-    console.log("MazoX: "+mazo.Xcoord);
-    console.log("MazoY: "+mazo.Ycoord);
-    console.log("RatonX: "+e.pageX);
-    console.log("RatonY: "+e.pageY);
     //Offset del canvas -> No hay puto offset a la hora de dibujar las cosas dentro del canvas..obviamente
     var offsetLeft = canvas.offsetLeft;
     var offsetTop = canvas.offsetTop;
+    //Mazo
     if ((e.pageX-offsetLeft > mazo.Xcoord)&&
     (e.pageX-offsetLeft< mazo.Xcoord + mazo.width)&&
     (e.pageY-offsetTop > mazo.Ycoord)&&
     (e.pageY-offsetTop< mazo.Ycoord + mazo.height)){
       console.log("Clicked");
+      //Robamos una carta
+      drawCard();
+      //Y de momento lo dibujamos al lado todo
+
     }
   });
 });
